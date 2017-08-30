43ed956c770e1549868c7d2310e3516a64928489
Nilesh <nileshgulia@gmail.com>
2017-08-30 22:15:34 +0530

    component life cycles

--- src/App.js
+++ src/App.js
@@ -4,152 +4,96 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      myText:"Welcome to react",
-      contentText:"this is react"
+
+    this.state = {
+      headerText: "Welcome to React!",
+      contentText: "In this lecture, we will go over the Components Life Cycle methods"
     };
   }
   render() {
-
     return (
       <div className="App">
-      {/*<h1>{this.state.myText}</h1>
-      <p>{this.state.contentText}</p>*/}
-         <Header header={this.state.myText}/>
-           <Content content={this.state.contentText}/>
-           <Clock />
+        {
+          /*
+          <h1>{this.state.headerText}</h1>
+          <p>{this.state.contentText}</p>
+          */
+        }
+        <Header />
+        <Content />
       </div>
     );
   }
 }
-App.defaultProps={
-  header:"this is default header",
-  content:"this is default content"
-}
-class Header extends Component{
+
+class Header extends Component {
   render() {
-    return(
+    return (
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Welcome to React</h2>
       </div>
-
     );
-
   }
 }
- {/*in react jsx classes are to be started with upeercase
-  if else dont work in jsx*/}
-class Content extends Component{
-  constructor(props){
+
+class Content extends Component {
+  constructor(props) {
     super(props);
-    this.state={
-      data: [],
-      count:0,
+
+    this.state = {
+      data: 0
     };
-    this.updateMyState=this.updateMyState.bind(this);{/*important, use it to bind 'this'to a function and use only in constructor*/ }
-    this.forceUpdateRandomNumber=this.forceUpdateRandomNumber.bind(this);
-    this.findMyDOMNode=this.findMyDOMNode.bind(this);
+    this.setNewNumber = this.setNewNumber.bind(this);
   }
-  updateMyState(){
-    var count=this.state.count;
-    count++;
-    var item="click ="+ count;
-    var myArr=this.state.data;
-    myArr.push(item);
-    this.setState({data:myArr,count:count});{/*used to update or set state*/}
-  }
-  forceUpdateRandomNumber(){
-    this.forceUpdate();{/*used to force update any state*/}
-  }
-  findMyDOMNode(){
-    var myDiv=document.getElementById('myDiv');
-    ReactDOM.findDOMNode(myDiv).style.color="red";{/*used to find any dom node and update them*/}
+  setNewNumber() {
+    this.setState({data: this.state.data + 1})
   }
   render() {
-    return(
-      <div>
-        <div className="App-intro">
-         <h1 >Components API</h1>
-          <p>I am creating components now</p>
-
-          </div>
-          <button onClick={this.updateMyState}>CLICK ME</button>
-          <h4>State data:{this.state.data}</h4>
-          <button onClick={this.forceUpdateRandomNumber}>Random number</button>
-          <h4>{Math.random()}</h4>
-            <button onClick={this.findMyDOMNode}>Find my DOM node</button>
-            <div id="myDiv">This is my div</div>
-          {/*<div>
-          <h4>Array:{this.props.propArray}</h4>
-          <h4>Bool:{this.props.propBoolean ? "True":"false"}</h4>
-          <h4>func:{this.props.propFunction(6)}</h4>
-          <h4>number:{this.props.propNumber}</h4>
-          <h4>string:{this.props.propString}</h4>
-          <h4>object:{this.props.propObject.objectname1}</h4>
-          <h4>object:{this.props.propObject.objectname2}</h4>
-          <h4>object:{this.props.propObject.objectname3}</h4>
-          </div>*/}
-          </div>
-
-
-
-
+    return (
+      <div className="App-intro">
+        <h1>Components Life Cycle!</h1>
+        <p>In this lecture, we will go over the Components Life Cycle methods</p>
+        <button onClick={this.setNewNumber}>Update Number</button>
+        <NumberComponent myNumber={this.state.data}/>
+      </div>
     );
-
   }
-}                    {/*remember this:React.PropTypes.datatypes*/}
-{/*Content.propTypes={
-  propArray:React.PropTypes.array.isRequired,
-  propBoolean:React.PropTypes.bool.isRequired,
-  propFunction:React.PropTypes.func,
-  propNumber:React.PropTypes.number,
-  propString:React.PropTypes.string,
-  propObject:React.PropTypes.object
 }
-Content.defaultProps={
-  propArray:[1,2,3,4],
-  propBoolean:true,
-  propFunction:function(e){return e},
-  propNumber:69,
-  propString:"whoaa",
-  propObject:{
-    objectname1:"ob1",
-    objectname2:"ob1",
-    objectname3:"ob1"
-  }
-}*/}
 
-         
-
-class Clock extends Component{
-  constructor(props){
-    super(props);
-    this.state={date:new Date(),comment:"hello"}
+class NumberComponent extends Component {
+  componentWillMount(){
+    console.log('Called the COMPONENT WILL MOUNT');
   }
   componentDidMount(){
-    this.timeID=setInterval(()=>this.tick(),1000);
+    console.log('Called the COMPONENT DID MOUNT');
   }
-    tick() {
-    this.setState({
-      date:new Date(),
-      comment:this.state.comment
-    });
+  componentWillReceiveProps(newProps){
+    console.log('Called the COMPONENT WILL RECEIVE PROPS');
+  }
+  shouldComponentUpdate(newProps, nextState) {
+   console.log('Called the SHOULD COMPONENT UPDATE');
+   return true;
+  }
+  componentWillUpdate(newProps, nextState) {
+    console.log('Called the COMPONENT WILL UPDATE');
+  }
+  componentDidUpdate(newProps, nextState) {
+    console.log('Called the COMPONENT DID UPDATE');
+  }
+  componentWillUnmount() {
+    console.log('Called the COMPONENT WILL UNMOUNT');
   }
   render() {
-    return(
-      <div>
-      <p>the time is:{this.state.date.toLocaleTimeString()}</p>
-      <p>{this.state.comment}</p>
-      </div>
-
-
-
+    return (
+      <h4>
+        {this.props.myNumber}
+      </h4>
     );
-
   }
-}
-
+}setTimeout(() => {
+	ReactDOM.unmountComponentAtNode(document.getElementById('root'));
+}, 10000)
 export default App;
