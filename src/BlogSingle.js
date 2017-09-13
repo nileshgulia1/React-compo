f8631f7095d5b9072e631504bfaf2d999c65741e
Nilesh <nileshgulia@gmail.com>
2017-09-13 11:59:25 -0700

    added blog components (routes)

---
+++ src/BlogSingle.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class BlogSingle extends Component {
+
+	render(){
+		return(
+			<h2>I am Single Blog</h2>
+			
+		);
+	}
+
+}
+
+export default BlogSingle;
