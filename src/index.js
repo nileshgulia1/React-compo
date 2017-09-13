1524020266b3f7f8e17da300c9911dfcedae212f
Nilesh <nileshgulia@gmail.com>
2017-09-14 00:27:25 +0530

    added router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import Blog from './Blog';
+import BlogSingle from './BlogSingle';
+import './index.css';
+import {
+	BrowserRouter as Router,
+	Route,
+	Link
+  } from 'react-router-dom'
+  
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Router >
+  	<Route path="/" component={App}>
+  		<Route path="/home" component={Home} />
+  		<Route path="/about" component={About} />
+  		<Route path="/contact" component={Contact} />
+  		<Route path="/blog" component={Blog}>
+  			<Route path="/blog/:postId" component={BlogSingle} />
+  		</Route>
+  	</Route>
+  </Router>
+  ,
+  document.getElementById('root')
+);
